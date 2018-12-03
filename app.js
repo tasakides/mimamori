81850e36a3e63672e6e568ebd6f785834ae0d0aa
田﨑太郎 <Tasaki@tasakitarou-no-MacBook-Air.local>
2018-12-04 01:02:29 +0900

    完成

---
+++ app.js
@@ -0,0 +1,40 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+let database
+
+(async function connect_mongo() {
+    let client = await MongoClient.connect('mongodb://localhost:27017')
+    database = client.db("IOT");
+    // let message = await database.collection("message").find().toArray()
+    // console.log(message)
+    // client.close()
+})()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + "/index.html");
+});
+
+app.get('/data/:id', async function (req, res) {
+    let id = req.params.id
+    let data = await database.collection("data").find({"id": id}).toArray()
+    res.send(data);
+});
+app.post("/data", async function (req, res) {
+    if (!("temp" in req.body) || !("id" in req.body)) {
+        res.send('ダメです');
+        return
+    }
+
+    database.collection("data").insertOne({"temp": req.body.temp, "id": req.body.id, "time": Date.now()})
+    res.send('OK');
+})
+app.listen(3000);
